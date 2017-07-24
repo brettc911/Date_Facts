287067baae2695357858363b25dac777723bdf5a
Brett Colyer <brettc911@gmail.com>
2017-07-24 16:48:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+const chalk = require('chalk');
+
+var leapYear = moment().isLeapYear()
+var dayLight = moment().isDST()
+
+var leap = (type)=>{
+  if (type == true) {
+    return "is"
+  } return "is not"
+}
+
+
+
+console.log(`It is ${chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))}.`)
+console.log(`It is the ${chalk.magenta(moment().format("DDDo"))} day of the year`)
+console.log(`It is ${chalk.cyan((moment().hour()*60*60)+(moment().minutes()*60)+(moment().seconds()))} seconds into the day`)
+console.log(`It ${chalk.green(leap(dayLight))} day light savings`)
+console.log(`It ${chalk.red(leap(leapYear))} a leap year`)
